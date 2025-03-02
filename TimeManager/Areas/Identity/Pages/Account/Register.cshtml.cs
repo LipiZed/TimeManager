d54dc9c80fb099fe26ffb1680563b740bd40fa19
“LipiZed” <kometa1810@gmail.com>
2025-03-02 15:15:56 +0300

    Changed requirements to register according to User model

--- TimeManager/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ TimeManager/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -71,33 +71,40 @@ namespace TimeManager.Areas.Identity.Pages.Account
         /// </summary>
         public class InputModel
         {
-            /// <summary>
-            ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-            ///     directly from your code. This API may change or be removed in future releases.
-            /// </summary>
             [Required]
             [EmailAddress]
             [Display(Name = "Email")]
             public string Email { get; set; }
 
-            /// <summary>
-            ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-            ///     directly from your code. This API may change or be removed in future releases.
-            /// </summary>
+            [Required]
+            [StringLength(50, ErrorMessage = "The {0} must be at most {1} characters long.")]
+            [Display(Name = "Login")]
+            public string Login { get; set; }
+
             [Required]
             [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
             [DataType(DataType.Password)]
             [Display(Name = "Password")]
             public string Password { get; set; }
 
-            /// <summary>
-            ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-            ///     directly from your code. This API may change or be removed in future releases.
-            /// </summary>
             [DataType(DataType.Password)]
             [Display(Name = "Confirm password")]
             [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
             public string ConfirmPassword { get; set; }
+
+            [Required]
+            [StringLength(50, ErrorMessage = "The {0} must be at max {1} characters long.")]
+            [Display(Name = "First Name")]
+            public string FirstName { get; set; }
+
+            [Required]
+            [StringLength(50, ErrorMessage = "The {0} must be at max {1} characters long.")]
+            [Display(Name = "Last Name")]
+            public string LastName { get; set; }
+
+            [StringLength(50, ErrorMessage = "The {0} must be at max {1} characters long.")]
+            [Display(Name = "Telegram ID")]
+            public string TelegramId { get; set; }
         }
 
 
@@ -115,8 +122,18 @@ namespace TimeManager.Areas.Identity.Pages.Account
             {
                 var user = CreateUser();
 
-                await _userStore.SetUserNameAsync(user, Input.Email, CancellationToken.None);
+                // Устанавливаем имя пользователя (Login) и электронную почту (Email) как разные значения
+                await _userStore.SetUserNameAsync(user, Input.Login, CancellationToken.None);
                 await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
+
+                // Устанавливаем значения дополнительных полей из формы в объект User
+                user.FirstName = Input.FirstName;
+                user.LastName = Input.LastName;
+                user.TelegramId = Input.TelegramId;
+
+                // Устанавливаем текущую дату и время при регистрации
+                user.RegistrationDate = DateTime.Now;
+
                 var result = await _userManager.CreateAsync(user, Input.Password);
 
                 if (result.Succeeded)
